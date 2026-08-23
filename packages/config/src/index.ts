84af329387399a61a37932dc309962d6e32a1c3d
Markus <hi@mrk.us>
2026-08-23 13:28:15 -0500

    chore(config): drop IS_PRODUCTION, dead since APP_URL was removed

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,4 +1,2 @@
-export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-
 export const APP_NAME = "Unremarkable";
 export const APP_DESCRIPTION = "Unremarkable is the best way to do nothing.";
